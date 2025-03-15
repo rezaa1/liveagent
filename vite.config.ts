c9c5ae151cbec450fb97153a6b241ec4e937409f
reza_azimi <reza.azimi@inferencesolutions.com>
2025-03-15 13:33:03 +1100

    add update docker file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist', // Ensure this matches the COPY command in Dockerfile
   },
 });
